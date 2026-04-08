35e6e21c05a06925208787d9e8896a4978e7242a
Thyago Neves <thyagoneves.sa@gmail.com>
2026-04-08 11:35:35 -0300

    fix: monorepo pnpm e conflitos de tipos para build na vercel

--- artifacts/barber-next/.next/dev/static/chunks/[root-of-the-server]__10j~f~y._.js
+++ artifacts/barber-next/.next/dev/static/chunks/[root-of-the-server]__10j~f~y._.js
@@ -469,7 +469,7 @@ function triggerUpdate(msg) {
 "[hmr-entry]/hmr-entry.js { ENTRY => \"[project]/artifacts/barber-next/pages/_error\" }", ((__turbopack_context__) => {
 "use strict";
 
-__turbopack_context__.r("[next]/entry/page-loader.ts { PAGE => \"[project]/node_modules/.pnpm/next@16.2.2_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/dist/pages/_error.js [client] (ecmascript)\" } [client] (ecmascript)");
+__turbopack_context__.r("[next]/entry/page-loader.ts { PAGE => \"[project]/artifacts/barber-next/node_modules/.pnpm/next@16.2.2_@babel+core@7.29.0_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/dist/pages/_error.js [client] (ecmascript)\" } [client] (ecmascript)");
 }),
 ]);
 
