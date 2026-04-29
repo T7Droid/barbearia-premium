13c8f0a595e7112f09ae2fcff025f49ef4b397e7
Thyago Neves <thyagoneves.sa@gmail.com>
2026-04-29 19:44:01 -0300

    ajusta pushs

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,6 @@
 const CACHE_NAME = 'kingbarber-v1';
 
 self.addEventListener('install', (event) => {
-  // PWA requirement: at least one fetch handler or cache something
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(['/']);
@@ -10,7 +9,6 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  // Minimal fetch handler to satisfy PWA requirements
   event.respondWith(
     fetch(event.request).catch(() => {
       return caches.match(event.request);
